0a7cf1d5e56938db298ad8f9a11f86fe56d801ad
zdocena <zahara.docena@gmail.com>
2010-06-02 16:48:20 -0700

    adding python exercises

---
+++ ex15.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "I'll also ask you to type it again 
---
+++ ex2.py
@@ -0,0 +1,6 @@
+print "I could have code like this." # and the comment after is ignored
+
+# more comments
+# more comments
+
+print "This will run."
